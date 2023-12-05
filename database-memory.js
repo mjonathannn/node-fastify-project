e35a850041bc31394dbd63f5736a94c0896f2100
Michael Jonathan <michaeljonathan_98@hotmail.com>
2023-12-05 20:13:00 -0300

    add mock data

--- database-memory.js
+++ database-memory.js
@@ -1,7 +1,30 @@
 import { randomUUID } from "crypto"
 
 export class DatabaseMemory {
-  #leads = new Map()
+  #leads = new Map([
+    [
+      randomUUID(),
+      {
+        cpf: "823.684.590-73",
+        name: "João da Silva",
+        maritalStatus: "solteiro(a)",
+        spouseName: "",
+        email: "joaodasilva@hotmail.com",
+        phone: "(12) 92321-4212",
+      },
+    ],
+    [
+      randomUUID(),
+      {
+        cpf: "235.713.720-77",
+        name: "Márcio dos Santos",
+        maritalStatus: "casado(a)",
+        spouseName: "Silvana Spohr",
+        email: "marciosantos@gmail.com",
+        phone: "(43) 29546-4129",
+      },
+    ],
+  ])
 
   list() {
     return Array.from(this.#leads.entries()).map((elem) => {
